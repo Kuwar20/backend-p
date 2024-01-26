2ce352fa1a7ecf323f4d3745628d1b07e234fded
Kuwar20 <kuwarx1@gmail.com>
2024-01-26 15:38:25 +0530

    Refactor phone number handling in PhoneLogin component

--- react-otplogin-fe/src/components/PhoneLogin.jsx
+++ react-otplogin-fe/src/components/PhoneLogin.jsx
@@ -1,20 +1,36 @@
 import React, { useState } from "react";
 
 const PhoneOtpLogin = () => {
-
     const [phoneNumber, setPhoneNumber] = useState("");
+    const [showOtpInput, setShowOtpInput] = useState(false);
+    
+    const handlePhoneNumber = (event) => {
+        // this will let us type in the input field
+        setPhoneNumber(event.target.value);
+    };
 
-    const handlePhoneNumber = () => {
+    const handlePhoneSubmit = (event) => {
+        // this will prevent the default submit behaviour
+        event.preventDefault();
 
-    }
+        //phone number validation
+        const regex = /^[0-9]{10}$/;
+        if (phoneNumber.length < 10 || !regex.test(phoneNumber)) {
+            alert("Please enter a valid phone number");
+            return;
+        }
+        //call the API to send the OTP
+        setShowOtpInput(true);
+    };
 
     return (
         <div>
-            <form onSubmit={() => { }}>
-                <input type="text" 
-                value={phoneNumber}
-                onChange={handlePhoneNumber}
-                placeholder="Enter your phone number"
+            <form onSubmit={handlePhoneSubmit}>
+                <input
+                    type="text"
+                    value={phoneNumber}
+                    onChange={handlePhoneNumber}
+                    placeholder="Enter your phone number"
                 />
                 <button type="submit">Submit</button>
             </form>
