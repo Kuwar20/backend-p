905034c9bc6b1fecf37f1f076068090e7dda58f3
Kuwar20 <kuwarx1@gmail.com>
2024-01-26 17:18:58 +0530

    Add functionality to move to next input and handle backspace key press

--- react-otplogin-fe/src/components/OtpInput.jsx
+++ react-otplogin-fe/src/components/OtpInput.jsx
@@ -27,10 +27,27 @@ const OtpInput = ({ length = 4, onOtpSubmit = () => { } }) => {
         const combinedOtp = newOtp.join("");
         //console.log(newOtp, combinedOtp);
         if (combinedOtp.length === length) onOtpSubmit(combinedOtp);
+
+        // move to next input
+        if (value.length === 1 && index !== length - 1) {
+            inputRefs.current[index + 1].focus();
+        }
     };
 
-    const handleClick = () => { };
-    const handleKeyDown = () => { };
+    const handleClick = (index) => {
+        inputRefs.current[index].setSelectionRange(1, 1);
+
+        //optional
+        if(index > 0 && !otp[index - 1]){
+            inputRefs.current[otp.indexOf("")].focus();
+        }
+    };
+
+    const handleKeyDown = (index, e) => {
+        if (e.key === "Backspace" && !otp[index] && index !== 0) {
+            inputRefs.current[index - 1].focus();
+        }
+    };
 
     return (
         <div>
