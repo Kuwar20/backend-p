64d861ecd5479d3404ed157638c2bca8b761d4be
Kuwar20 <kuwarx1@gmail.com>
2024-01-26 15:17:42 +0530

    Add PhoneOtpLogin component and update App.css

--- react-otplogin-fe/src/App.jsx
+++ react-otplogin-fe/src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import "./App.css";
+import PhoneOtpLogin from "./components/PhoneLogin";
 
 function App() {
-
+  
   return (
-    <div>
-      <p>Hello</p>
+    <div className="App">
+      <h1>Login with Phone</h1>
+      <PhoneOtpLogin />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ react-otplogin-fe/src/components/PhoneLogin.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PhoneOtpLogin = () => {
+  return <div>PhoneOtpLogin</div>;
+};
+
+export default PhoneOtpLogin;
