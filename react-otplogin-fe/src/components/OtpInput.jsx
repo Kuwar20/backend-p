158c957218078de32a079ccc1b30da016d886bd6
Kuwar20 <kuwarx1@gmail.com>
2024-01-26 16:11:38 +0530

    Refactor CSS and add OTP input component

--- react-otplogin-fe/src/components/OtpInput.jsx
+++ react-otplogin-fe/src/components/OtpInput.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const OtpInput = ({ length = 4, onOtpSubmit = () => { } }) => {
-    return (
-        <div>OtpInput</div>
-    )
-}
+    const [otp, setOtp] = useState(new Array(length).fill(""));
 
-export default OtpInput
+    const handleChange = () => { };
+    const handleClick = () => { };
+    const handleKeyDown = () => { };
+
+    return (
+        <div>
+            {otp.map((value, index) => {
+                return (
+                    <input
+                        key={index}
+                        type="text"
+                        value={value}
+                        onChange={(e) => handleChange(index, e)}
+                        onClick={() => handleClick(index)}
+                        onKeyDown={(e) => handleKeyDown(index, e)}
+                        className="otpInput"
+                    />
+                );
+            })}
+        </div>
+    );
+};
+
+export default OtpInput;
