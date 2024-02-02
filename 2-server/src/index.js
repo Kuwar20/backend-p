cd274dc2e2c2a3e48bf6fc5c9371a20862d89f9d
Kuwar20 <kuwarx1@gmail.com>
2024-02-02 11:22:12 +0530

    Add cors package and enable CORS

--- 2-server/src/index.js
+++ 2-server/src/index.js
@@ -2,8 +2,12 @@ import express from "express";
 const app = express();
 import userRoutes from "./routes/userRoute.js";
 import { connectDB } from "./utils/features.js";
+import cors from "cors";
 
 connectDB();
+
+// to allow the frontend to access the backend
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 // to send json data to the mongodb database
 app.use(express.json());
