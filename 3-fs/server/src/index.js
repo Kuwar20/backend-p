e4612a31c81f47a8c0ef327ba78df6a20c003ba6
Kuwar20 <kuwarx1@gmail.com>
2024-02-04 17:02:06 +0530

    Add dev script and dependencies to package.json

---
+++ 3-fs/server/src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+const app = express();
+import userRoutes from "./routes/userRoute.js";
+import { connectDB } from "./utils/features.js";
+import cors from "cors";
+
+connectDB();
+
+app.use(cors({ origin: 'http://localhost:5173' }));
+
+// to send json data to the mongodb database
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('server is running');
+})
+
+app.use('/api/user', userRoutes)
+
+app.listen(3000, () => {
+    console.log('server on port 3000');
+})
