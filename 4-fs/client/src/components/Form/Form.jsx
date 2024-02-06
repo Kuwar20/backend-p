70c15514b67f2e702430784f8c38587fbee56881
Kuwar20 <kuwarx1@gmail.com>
2024-02-06 12:26:11 +0530

    Add form validation and submission functionality

--- 4-fs/client/src/components/Form/Form.jsx
+++ 4-fs/client/src/components/Form/Form.jsx
@@ -1,23 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Form.css'
 
+
 const Form = () => {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [error, setError] = useState("");
+  const [successMessage, setSuccessMessage] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      setError("Please enter name");
+      return;
+    }
+    if (!email) {
+      setError("Please enter email");
+      return;
+    }
+    if (!number) {
+      setError("Please enter number");
+      return;
+    }
+    if(number.length !== 10){
+      setError("Number must be 10 digits")
+      return
+    }
+    setError("");
+    console.log(name, email, number);
+    setSuccessMessage("Form Submitted successfully")
+
+    setTimeout(()=>{
+      setName('');
+      setEmail('');
+      setNumber('');
+      setSuccessMessage('');
+      setError('');
+    },4500);
+  }
+
   return (
     <div className='form'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Name:
-          <input type="text"
+          <input
+            type="text"
             placeholder='Name'
-            />
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </label>
         <br />
         <label>Email:
-          <input type="email"
+          <input
+            type="email"
             placeholder='Email'
-            />
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <br />
-          <button>Submit</button>
+        <label> Number:
+          <input
+            type="number"
+            placeholder='Phone Number'
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+          />
+        </label>
+        <br />
+        {error && <p style={{ color: "red", fontWeight:"bold", textAlign:"center" }}>{error}</p>}
+        <button>Submit</button>
+        {successMessage && <p style={{ color: "green", fontWeight:"bold", textAlign:"center" }}>{successMessage}</p>}
       </form>
     </div>
   )
