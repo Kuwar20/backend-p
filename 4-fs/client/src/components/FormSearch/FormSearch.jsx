9473f4f2d01c8d363a0173543c0853d8d6173852
Kuwar20 <kuwarx1@gmail.com>
2024-02-07 10:26:57 +0530

    Add input validation and API calls in FormSearch component

--- 4-fs/client/src/components/FormSearch/FormSearch.jsx
+++ 4-fs/client/src/components/FormSearch/FormSearch.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./FormSearch.css"
 
 const FormSearch = () => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const isNumber = !isNaN(parseFloat(inputValue)) && isFinite(inputValue);
+        
+        if(isNumber){
+            //call api for number search
+            fetchNumberAPI(inputValue);
+        }
+        else{
+            //call api for number search
+            fetchNameAPI(inputValue);
+        }
+    }
+    
+    const fetchNumberAPI = (number) => {
+        console.log(`Fetching number API for: ${number}`);
+    }
+    const fetchNameAPI = (name) => {
+        console.log(`Fetching name API for: ${name}`);
+    }
+
     return (
         <div className='form-search'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     Enter whatever you remember:
                     <input
                         type="text"
                         placeholder='Enter Detail'
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
                     />
                 </label>
                 <br />
