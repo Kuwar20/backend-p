89601842ddb75328fca4fc6c07227b1a4aa00256
Kuwar20 <kuwarx1@gmail.com>
2024-02-09 23:32:17 +0530

    Add header navigation and styling

--- 5-sinlog/client/src/components/Header/Header.jsx
+++ 5-sinlog/client/src/components/Header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './Header.css'
+import {NavLink} from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <nav>
+        <ul className='option'>
+          <li className='right'>
+            <NavLink to='/'>Home</NavLink>
+            </li>
+          <li className='left'>
+            <NavLink to='/login'>Login</NavLink>
+          </li>
+          <li className='left'>
+            <NavLink to='/signup'>Signup</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
