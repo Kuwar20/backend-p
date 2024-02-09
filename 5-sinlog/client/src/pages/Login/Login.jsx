4bfe94e6ca73cc0b5213a4615f2970e17241aeaa
Kuwar20 <kuwarx1@gmail.com>
2024-02-09 23:55:26 +0530

    Add login form styling and functionality

--- 5-sinlog/client/src/pages/Login/Login.jsx
+++ 5-sinlog/client/src/pages/Login/Login.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './Login.css'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='login-form'>
+      <form>
+        <label>Name:
+          <input 
+              type="text"
+              placeholder='Enter your name'
+            />
+        </label>
+        <br />
+        <label>Password:
+          <input 
+                type="text"
+                placeholder='Enter your password'
+                />
+        </label>
+        <br />
+        <button>Login</button>
+        <Link to='/signup'><p>Create Account?</p></Link>
+      </form>
+    </div>
   )
 }
 
