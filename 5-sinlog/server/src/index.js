43999b2b4e4f076a0e61cdba8eae521e77fd264b
Kuwar20 <kuwarx1@gmail.com>
2024-02-10 12:06:23 +0530

    Update response in index.js

--- 5-sinlog/server/src/index.js
+++ 5-sinlog/server/src/index.js
@@ -4,7 +4,25 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+
+    res.status(201).send('Hello World'); // sends a string
+    //o/p - Hello Worlds 
+
+    /*
+        res.status(201).send({ message: 'Hello World' }); // sends a JSON string
+        o/p - {"message": "Hello World"} 
+    */
+
+    /*  
+        res.status(201).json({ message: 'Hello World' });
+        o/p - {"message": "Hello World"} 
+    */
+
+    /*
+        res.status(201).json('Hello World');
+        o/p - {"message": "Hello World"}
+    */
+    
 });
 
 app.listen(PORT, () => {
