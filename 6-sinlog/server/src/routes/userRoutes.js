663d46ad677a2999de879063dfe5778c7d0a2d66
Kuwar20 <kuwarx1@gmail.com>
2024-02-18 00:44:12 +0530

    Refactor userRoutes.js: Update registration and login functionality

--- 6-sinlog/server/src/routes/userRoutes.js
+++ 6-sinlog/server/src/routes/userRoutes.js
@@ -31,16 +31,55 @@ router.post('/register', async (req, res) => {
         })
 
         await newUser.save();
-        res.status(201).send('User registered successfully');
+        res.status(201).send('User created successfully');
     } catch (error) {
         console.log(error);
         res.status(500).send('Something went wrong');
     }
 })
 
+/* 
+    this is not actually login, it does somethings in the backend when we hit this api with email and password,
+    then it returns a message "User logged in successfully" if everything is fine
+    this does not actually login the user, it just checks if the user exists in the db and if the password is correct
+ */
 router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-    // validate input fields
+
+    // for 'login' we use body because we are sending sensitive data like password, and same for the 'register api'
+    /*     
+        //get email and password from the request body
+        
+        const { email, password } = req.body;
+        its frontend will be:
+            const response = await fetch(`http://localhost:3000/api/user/login`, {
+            method: 'POST',
+            headers: {
+                    'Content-Type': 'application/json'
+                    },
+                body: JSON.stringify({ email, password })
+                })
+                const data = await response.json(); 
+    */
+
+    /*
+        // or get email and password from the query parameters / api url
+        
+        const { email, password } = req.query;
+        its frontend will be:
+        const response = await fetch(`http://localhost:3000/api/user/login?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`, {
+        method: 'POST',
+        })
+     */
+
+    // this is the mix of both, it will first check the request body and if email and password are not found in the request body, then it will check the query parameters
+    // and for frontend it will use way 1
+    let { email, password } = req.body; // Extract email and password from the request body
+    // If email and password are not found in the request body,then check the query parameters
+    if (!email || !password) {
+        email = req.query.email;
+        password = req.query.password;
+    }
+
     if (!email || !password) {
         return res.status(400).send('All input is required');
     }
@@ -53,7 +92,7 @@ router.post('/login', async (req, res) => {
         if (!isMatch) {
             return res.status(400).send('Invalid credentials');
         }
-        
+
         res.send('User logged in successfully');
     } catch (error) {
         console.log(error);
