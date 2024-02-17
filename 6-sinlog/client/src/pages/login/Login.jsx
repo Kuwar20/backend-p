17ac768d3bae64561808bd9ac4b35bdb5c35d78e
Kuwar20 <kuwarx1@gmail.com>
2024-02-17 18:00:33 +0530

    Add login form styling

--- 6-sinlog/client/src/pages/login/Login.jsx
+++ 6-sinlog/client/src/pages/login/Login.jsx
@@ -3,7 +3,27 @@ import './Login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='login'>
+      <h2>Login</h2>
+      <form>
+        <label>Email:
+          <input 
+            type="text"
+            placeholder='Enter your name'
+            />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input 
+            type="password"
+            placeholder='Enter your password'
+            />
+        </label>
+        <br />
+        <button type='submit'>Login</button>
+      </form>
+    </div>
   )
 }
 
