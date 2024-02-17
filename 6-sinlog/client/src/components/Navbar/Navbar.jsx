8873fb4b4e9d05878bbb069a91f788ff5c7c663a
Kuwar20 <kuwarx1@gmail.com>
2024-02-17 11:40:36 +0530

    Add navbar component with styling

--- 6-sinlog/client/src/components/Navbar/Navbar.jsx
+++ 6-sinlog/client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='navbar-left'>
+        <NavLink to='/'>Home</NavLink>
+      </div>
+      <div className='navbar-right'>
+        <NavLink to='/login'>Login</NavLink>
+        <NavLink to='/register'>Register</NavLink>
+      </div>
+    </div>
   )
 }
 
