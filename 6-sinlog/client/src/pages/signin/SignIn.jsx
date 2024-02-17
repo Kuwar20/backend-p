e4a385ed0cfa9f6c821f2a642bb94525bac4d712
Kuwar20 <kuwarx1@gmail.com>
2024-02-17 17:45:19 +0530

    Add registration form to SignIn page

--- 6-sinlog/client/src/pages/signin/SignIn.jsx
+++ 6-sinlog/client/src/pages/signin/SignIn.jsx
@@ -3,7 +3,43 @@ import './SignIn.css'
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <div className='register'>
+      <h1>Sign In</h1>
+      <form>
+        <label>Full Name:
+          <input 
+            type="text"
+            placeholder='Enter your Name'
+            />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input 
+            type="email"
+            placeholder='Enter your Email'
+          />
+        </label>
+        <br />
+        <label>Gender: 
+        <select className='options'>
+          <option value='' disabled>Gender</option>
+          <option value='Male'>Male</option>
+          <option value='Female'>Female</option>
+          <option value='Other'>Other</option>
+        </select>
+        </label>
+        <br />
+        <label>Password:
+          <input 
+            type="password"
+            placeholder='Enter your Password'
+          />
+        </label>
+        <br />
+        <button>Submit</button>
+      </form>
+    </div>
   )
 }
 
