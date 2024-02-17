7c228739f535bf97cf1e25e72902e76328ca47df
Kuwar20 <kuwarx1@gmail.com>
2024-02-17 18:19:11 +0530

    Add test API endpoint

--- 6-sinlog/server/src/index.js
+++ 6-sinlog/server/src/index.js
@@ -3,6 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req,res)=>{
+    res.status(201).send('Test API');
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
 })
