52c88ce7bd24b1aa7e5372105a1f1c524cec77c6
Kuwar20 <kuwarx1@gmail.com>
2024-02-17 23:11:00 +0530

    Add userSchema and import in index.js

--- 6-sinlog/server/src/index.js
+++ 6-sinlog/server/src/index.js
@@ -4,6 +4,12 @@ const app = express();
 import { connectDB } from './utils/conn.js'; 
 connectDB(); // connect to database
 
+/* 
+// (PART 3/3) - importing the userSchema from the user.js file, PART 1 & 2 are in the user.js file in the models folder
+// so that we can see how the schema works in the db
+import userSchema from './models/user.js';
+*/
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req,res)=>{
---
+++ 6-sinlog/server/src/models/user.js
@@ -0,0 +1,71 @@
+import mongoose from 'mongoose';
+
+/*
+(PART 1/3)
+// checking how this schema works in the db 
+
+import { connectDB } from '../utils/conn.js';
+connectDB();
+
+*/
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    gender:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    },
+    updatedAt:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+/* 
+    This line creates a Mongoose model from the User schema.
+    Note that Mongoose will lowercase the name and pluralize it. So in our MongoDB database, 
+    the collection will actually be named "users", not "User". 
+*/
+
+/*
+// checking how this schema works in the db (PART 2/3)
+
+// Create a new user with hardcoded values in db from here
+const user = new User({
+    name: 'Test User',
+    email: 'test@example.com',
+    password: 'password123',
+    gender:'male'
+});
+console.log('About to save user');
+
+// Save the user to the database
+user.save()
+    .then(doc => {
+        console.log('User saved successfully', doc);
+    })
+    .catch(err => {
+        console.error('Error saving user', err);
+    });
+
+*/
+
+export default User;
