9b61fc8c8d2dfef66728b9a93d2fb4eb5c1288e1
Kuwar20 <kuwarx1@gmail.com>
2024-02-23 12:08:48 +0530

    Add react-router-dom dependency and update App.jsx

--- 7-sinlog/client/src/App.jsx
+++ 7-sinlog/client/src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signin from './pages/signup/Signup'
+import Headers from './components/header/Header'
 
 function App() {
 
   return (
     <>
-      <h1>hello</h1>
+      <BrowserRouter>
+      <Headers />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Signin />} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
