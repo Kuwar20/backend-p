3b57bbb31e5c9ee73794b13dcb8bab2af104821b
Kuwar20 <kuwarx1@gmail.com>
2024-03-04 10:07:44 +0530

    added ui to navigation

--- 9-contactpage/client/src/components/Navbar.jsx
+++ 9-contactpage/client/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import photo from '../../public/images/Service 24_7-pana 1.svg'
 import logo from '../../public/images/brand_logo.png'
+import './Navbar.css'
 
 const Navbar = () => {
     return (
         <nav>
-            <div className='logo'>
+            <div className='logo container'>
                 <img src={logo} alt="Photo" />
             </div>
 
