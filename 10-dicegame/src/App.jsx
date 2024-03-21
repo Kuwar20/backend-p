3cdce3734b37c6007c59fece37c3711edc92c93f
Kuwar20 <kuwarx1@gmail.com>
2024-03-21 13:48:08 +0530

    Add game play functionality to App component

--- 10-dicegame/src/App.jsx
+++ 10-dicegame/src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
+import { useState } from 'react';
 import styled from 'styled-components';
 import StartGame from './components/StartGame';
+import GamePlay from './components/GamePlay';
 
 function App() {
 
+  const [isGameStarted, setIsGameStarted] = useState(false)
+  
+  const toggleGamePlay = () => {
+    setIsGameStarted((prev) => !prev);
+  }
+
   return (
     <>
-      <StartGame />
+    {isGameStarted ? <GamePlay /> : <StartGame toggle={toggleGamePlay} />}
     </>
   )
 }
--- 10-dicegame/src/components/StartGame.jsx
+++ 10-dicegame/src/components/StartGame.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import image1 from '../../src/assets/dices 1.png'
 
-const StartGame = () => {
+const StartGame = ({toggle}) => {
   return (
     <Container>
       <div>
@@ -10,7 +10,7 @@ const StartGame = () => {
       </div>
       <div className='content'>
         <h1>DICE GAME</h1>
-        <Button>Play Now</Button>
+        <Button onClick={toggle}>Play Now</Button>
       </div>
     </Container>
   )
