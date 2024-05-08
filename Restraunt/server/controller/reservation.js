414b10704ba6866fc69818629b0824c3481bb03c
Kuwar20 <kuwarx1@gmail.com>
2024-05-08 12:35:24 +0530

    chore: Import express in reservation controller

--- Restraunt/server/controller/reservation.js
+++ Restraunt/server/controller/reservation.js
@@ -1,3 +1,4 @@
+import e from "express";
 import ErrorHandler from "../error/error.js";
 import { Reservation } from "../models/reservationSchema.js";
 
@@ -25,5 +26,6 @@ export const sendReservation = async (req, res, next) => {
             const validationErrors = Object.values(error.errors).map(err => err.message);
             return next(new ErrorHandler(validationErrors.join(', '), 400));
         }
+        return next(error);
     }
 };
