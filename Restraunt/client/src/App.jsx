f7bcd088ec15bdfad13c0355c530f222f9ee2f76
Kuwar20 <kuwarx1@gmail.com>
2024-05-10 10:52:18 +0530

    Frontend init

---
+++ Restraunt/client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  )
+}
+
+export default App
