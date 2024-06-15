b7ad3c816d9810aaaabc36477ae1593ea6f6996b
Kuwar20 <kuwarx1@gmail.com>
2024-06-15 23:23:08 +0530

    chore: Add Redux store configuration and counterSlice reducer

--- react-redux-ex/src/components/Navbar.jsx
+++ react-redux-ex/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
-const Navbar = ({ count }) => {
+const Navbar = () => {
+    const count = useSelector((state) => state.counter.value)
+
     return (
         <div>We are passing data from parent components 'app.jsx' to child component 'navbar.jsx'.
             the number of count is: {count}</div>
