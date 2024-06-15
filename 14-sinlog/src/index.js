0d80a715f097c3c5f64fde8e604ef40db5f247e5
Kuwar20 <kuwarx1@gmail.com>
2024-06-15 17:35:32 +0530

    Connect DB to express server

--- 14-sinlog/src/index.js
+++ 14-sinlog/src/index.js
@@ -1,6 +1,12 @@
 import express from "express";
 const app = express();
 
+import dotenv from "dotenv";
+dotenv.config();
+
+import { connectDB } from "./utils/connDB.js";
+connectDB();
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
