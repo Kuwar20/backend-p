a234691e3404799e37c7a902cefb5dc72561aaf1
Kuwar20 <kuwarx1@gmail.com>
2024-06-15 17:44:40 +0530

    feat: Add user schema and model

--- 14-sinlog/src/index.js
+++ 14-sinlog/src/index.js
@@ -7,6 +7,11 @@ dotenv.config();
 import { connectDB } from "./utils/connDB.js";
 connectDB();
 
+/* 
+// (PART 3/3) - importing the userSchema from the user.js file, PART 1 & 2 are in the user.js file in the models folder
+// so that we can see how the schema works in the db
+import userSchema from './models/user.js';
+*/
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
