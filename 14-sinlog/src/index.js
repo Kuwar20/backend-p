c232228aa0ef94a1a4e8dd4de2296dae223cf315
Kuwar20 <kuwarx1@gmail.com>
2024-06-15 17:07:05 +0530

    chore: Update server port configuration

--- 14-sinlog/src/index.js
+++ 14-sinlog/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
