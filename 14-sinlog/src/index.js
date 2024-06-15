a51861bec00d4b925c5d8a68f3895b6db59dd9fb
Kuwar20 <kuwarx1@gmail.com>
2024-06-15 20:32:41 +0530

    Add Route (API) on backend

--- 14-sinlog/src/index.js
+++ 14-sinlog/src/index.js
@@ -7,6 +7,8 @@ dotenv.config();
 import { connectDB } from "./utils/connDB.js";
 connectDB();
 
+import userRoutes from "./routes/userRoutes.js";
+
 /* 
 // (PART 3/3) - importing the userSchema from the user.js file, PART 1 & 2 are in the user.js file in the models folder
 // so that we can see how the schema works in the db
@@ -14,10 +16,17 @@ import userSchema from './models/user.js';
 */
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+/* 
+ without this line of code, the server will not be able to read the request body,
+ and give error => TypeError: Cannot destructure property 'name' of 'req.body' as it is undefined.
+*/
 
 app.get("/", (req, res) => {
     res.send("Test api");
 });
+
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
---
+++ 14-sinlog/src/routes/userRoutes.js
@@ -0,0 +1,80 @@
+import express from "express";
+import User from '../models/user.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = 'SECRET_KEY';
+const router = express.Router();
+
+router.get('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+    if (password.length < 6) {
+        return res.status(400).json({ message: "Password must be at least 6 characters long" });
+    }
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        await user.save();
+        res.status(200).json({ message: "User registered successfully" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+})
+
+
+router.post('/login', async (req, res) => {
+    // make this let instead of const, so that when we dont send any value in the request body, we can still check the query parameters,
+    // in case we have email and password as const, and we dont send email and password in the request body, then we will get an error and it will stop the server "Assignment to constant variable"
+    // this wont happen if we use let
+    let { email, password } = req.body; // Extract email and password from the request body
+
+    // If email and password are not found in the request body,then check the query parameters
+    if (!email || !password) {
+        email = req.query.email;
+        password = req.query.password;
+    }
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const token = jwt.sign({email:user.email}, process.env.JWT_SECRET, {expiresIn: '1h'});
+        // const token = jwt.sign({email:user.email}, JWT_SECRET, {expiresIn: '1h'}); // or use this without the .env file
+
+        res.status(200).json({ message: "Login successful", token });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+})
+
+
+export default router;
