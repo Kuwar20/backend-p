de40465d8730cc31d588d6795aa3c1efb8b421c5
Kuwar20 <kuwarx1@gmail.com>
2024-06-27 20:33:03 +0530

    feat: Add NotFound component and route in App.jsx
    The code changes in this commit add the NotFound component and route in App.jsx. The NotFound component is responsible for rendering a "Not Found" message when a user navigates to a non-existent route. This improves the user experience by providing a clear indication when a page is not available.

--- 15-reactbackend/src/App.jsx
+++ 15-reactbackend/src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Form from './components/Form';
+import NotFound from './Pages/Notfound';
 function App() {
-  
+
 
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Form />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
