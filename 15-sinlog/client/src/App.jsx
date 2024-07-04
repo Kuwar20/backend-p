f8e3a62ea2b88104f42c5aed975fdc8d344cc2cf
Kuwar20 <kuwarx1@gmail.com>
2024-07-04 09:27:37 +0530

    Made Responsive Navbar

--- 15-sinlog/client/src/App.jsx
+++ 15-sinlog/client/src/App.jsx
@@ -1,10 +1,24 @@
+import {BrowserRouter,Routes, Route} from 'react-router-dom';
 import './App.css'
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Search from './pages/Search';
 
 function App() {
 
   return (
     <>
-      <h1 className='text-2xl'>Hello world</h1>
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/search" element={<Search/>} />
+        <Route path='*' element={<h1>Not Found</h1>} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
