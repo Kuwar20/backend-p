4f5fdba13f4381e4a5490bb8eec605540532ea2d
Kuwar20 <kuwarx1@gmail.com>
2024-07-04 13:44:37 +0530

    Login ui

--- 15-sinlog/client/src/pages/Login.jsx
+++ 15-sinlog/client/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import toast,{Toaster} from 'react-hot-toast'
+import toast, { Toaster } from 'react-hot-toast'
 
 const Login = () => {
   const [email, setEmail] = useState('')
@@ -35,53 +35,52 @@ const Login = () => {
   }
 
   return (
-    <div>
-      {issubmitted ? (<div><p>Loading...</p></div>
-      ):(
-        <>
-              <div>
-        <p>Login</p>
-      </div>
-      <div>
-        <div>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label>Email</label>
-            </div>
-            <div>
-              <input 
-                type="email"
-                placeholder='Enter your email address'
-                required
-                suggested="email"
-                value={email}
-                onChange={(e)=>setEmail(e.target.value)}
-                />
-            </div>
-            <div>
-              <label>Password</label>
-            </div>
-            <div>
-              <input 
-                type="password"
-                placeholder='Enter your password'
-                required
-                suggested="current-password"
-                value={password}
-                onChange={(e)=>setPassword(e.target.value)}
+    <div className='min-h-screen bg-slate-200 flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
+          <div className='sm:mx-auto sm:w-full sm:max-w-md'>
+            <p className='font-medium text-center text-blue-600 hover:text-blue-500'>Login</p>
+          </div>
+          <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+            <div className=' bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10'>
+              <form onSubmit={handleSubmit} className='space-y-4'>
+                <div className='block text-sm font-medium text-gray-700'>
+                  <label>Email</label>
+                </div>
+                <div className='mt-2'>
+                  <input
+                    type="email"
+                    placeholder='Enter your email address'
+                    required
+                    suggested="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className='appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
                   />
+                </div>
+                <div className='block text-sm font-medium text-gray-700'>
+                  <label>Password</label>
+                </div>
+                <div className='mt-2'>
+                  <input
+                    type="password"
+                    placeholder='Enter your password'
+                    required
+                    suggested="current-password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className='appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                  />
+                </div>
+                <div className='mt-1'>
+                  <button
+                    disabled={issubmitted}
+                    type='submit'
+                    className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >{issubmitted ? 'Loading...' : 'Submit'}</button>
+                </div>
+              </form>
+              <Toaster />
             </div>
-            <div>
-              <button
-                disabled={issubmitted} 
-              type='submit'>{issubmitted?'Loading...':'Submit'}</button>
-            </div>
-          </form>
-          <Toaster />
-        </div>
-      </div>
-        </>
-      )}
+          </div>
     </div>
   )
 }
