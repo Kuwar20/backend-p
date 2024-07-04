8d148013d5957d6b379d5d459cbc2b476a9546e5
Kuwar20 <kuwarx1@gmail.com>
2024-07-04 11:17:16 +0530

    heading positioned to center

--- 15-sinlog/client/src/pages/Signup.jsx
+++ 15-sinlog/client/src/pages/Signup.jsx
@@ -14,7 +14,7 @@ const Signup = () => {
   return (
     <div className='min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
       <div className='sm:mx-auto sm:w-full sm:max-w-md'>
-        <p className='font-medium text-blue-600 hover:text-blue-500'>Create your Account</p>
+        <p className='font-medium text-center text-blue-600 hover:text-blue-500'>Create your Account</p>
       </div>
 
       <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
