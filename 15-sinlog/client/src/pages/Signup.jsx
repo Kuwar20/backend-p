4193b8b9ec3ee2c36b75a69631871c890a8614c9
Kuwar20 <kuwarx1@gmail.com>
2024-07-04 10:37:48 +0530

    Responsive Signup Page in Tailwind

--- 15-sinlog/client/src/pages/Signup.jsx
+++ 15-sinlog/client/src/pages/Signup.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(name,email)
+  }
+
   return (
-    <div>Signup</div>
+    <div className='min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
+      <div className='sm:mx-auto sm:w-full sm:max-w-md'>
+        <p className='font-medium text-blue-600 hover:text-blue-500'>Create your Account</p>
+      </div>
+
+      <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+        <div className='bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10'>
+          <form className='space-y-6' onSubmit={handleSubmit}>
+            
+            <div>
+              <label className='block text-sm font-medium text-gray-700'>
+                Name
+              </label>
+            </div>
+            <div className='mt-1'>
+              <input 
+                type="text"
+                placeholder='Enter your name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+                className='appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                />
+            </div>
+
+            <div>
+              <label className='block text-sm font-medium text-gray-700'>
+                Email 
+              </label>
+            </div>
+            <div className='mt-1'>
+              <input 
+                type="email" 
+                placeholder='Enter your email address'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className='appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                />
+            </div>
+            <div className='text-sm'>
+              <Link to=''
+                className='font-medium text-blue-600 hover:text-blue-500'>Forget Your Password</Link>
+            </div>
+            <div>
+              <button type='submit'
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >Signup</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
