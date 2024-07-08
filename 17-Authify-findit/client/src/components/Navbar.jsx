ae03d99163ea4ee5b4810c23f1aadc2dda653563
Kuwar20 <kuwarx1@gmail.com>
2024-07-08 21:16:08 +0530

    Added icon instead of svg

--- 17-Authify-findit/client/src/components/Navbar.jsx
+++ 17-Authify-findit/client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { HiBars3BottomRight } from "react-icons/hi2";
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -17,9 +18,7 @@ const Navbar = () => {
                 </div>
                 <div className='md:hidden'>
                     <button onClick={() => setIsOpen(!isOpen)} className='text-white'>
-                        <svg className='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-                            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16m-7 6h7'}></path>
-                        </svg>
+                        <HiBars3BottomRight  className='w-6 h-7'/>
                     </button>
                 </div>
             </div>
