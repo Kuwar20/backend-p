b337b76865a0d0c366ceaf83de650560d08b93dd
Kuwar20 <kuwarx1@gmail.com>
2024-07-09 22:24:46 +0530

    winston morgan advance logger added

--- 17-Authify-findit/server/src/index.js
+++ 17-Authify-findit/server/src/index.js
@@ -1,7 +1,30 @@
 import express from 'express';
 const app = express();
 
+import dotenv from 'dotenv';
+dotenv.config();
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+import logger from '../logger.js';
+import morgan from 'morgan';
+
+const morganFormat = ':method :url :status :response-time ms';
+
+app.use(morgan(morganFormat, {
+    stream: {
+        write: (message) => {
+            const logObject = {
+                method: message.split(' ')[0],
+                url: message.split(' ')[1],
+                status: message.split(' ')[2],
+                responseTime: message.split(' ')[3],
+            };
+            logger.info(JSON.stringify(logObject));
+        }
+    }
+}));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
