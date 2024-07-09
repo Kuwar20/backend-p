af9412a35dbd22d79555eb78fc2e2adb1ca8af4a
Kuwar20 <kuwarx1@gmail.com>
2024-07-09 11:04:47 +0530

    files added to route

--- 17-Authify-findit/client/src/App.jsx
+++ 17-Authify-findit/client/src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Search from './pages/Search'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -10,6 +13,9 @@ function App() {
       <Navbar/>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/search' element={<Search />} />
         </Routes>
       </BrowserRouter>
     </>
