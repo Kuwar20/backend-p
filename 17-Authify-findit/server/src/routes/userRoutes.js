96b1864ef0b4141a8797f1784863affce007e27a
Kuwar20 <kuwarx1@gmail.com>
2024-07-10 21:44:52 +0530

    login route api created

--- 17-Authify-findit/server/src/routes/userRoutes.js
+++ 17-Authify-findit/server/src/routes/userRoutes.js
@@ -23,9 +23,36 @@ router.post('/register', async (req, res) => {
             return res.status(422).json({ error: "User already exists" });
         }
         const hashedPassword = await bcrypt.hash(password, 12);
-        const newUser = new User({ firstName, lastName, email, password: hashedPassword});
+        const newUser = new User({ firstName, lastName, email, password: hashedPassword });
         await newUser.save();
         res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+router.post('/login', async (req, res) => {
+    const { email, password, ...additionalItems } = req.body;
+
+    if (!email || !password) {
+        return res.status(422).json({ error: "Please fill all the fields" });
+    }
+
+    if (Object.keys(additionalItems).length > 0) {
+        return res.status(422).json({ error: "Please fill only the required fields" });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(422).json({ error: "Invalid credentials" });
+        }
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(422).json({ error: "Invalid credentials" });
+        }
+        res.status(200).json({ message: "User logged in successfully" });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Internal server error" });
