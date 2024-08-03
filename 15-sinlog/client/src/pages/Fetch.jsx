4a69e4017b4d949819245470cd611b50a55961f3
Kuwar Singh <66473902+Kuwar20@users.noreply.github.com>
2024-08-03 10:37:32 +0000

    chore:UI created

--- 15-sinlog/client/src/pages/Fetch.jsx
+++ 15-sinlog/client/src/pages/Fetch.jsx
@@ -2,7 +2,21 @@ import React from "react";
 
 const Fetch = () => {
     return (
-        <h1>Fetch user data</h1>
+        <div>
+            <h1>Fetch user data</h1>
+            <div>
+                <h1>Search all</h1>
+            </div>
+            <div>
+                <input
+                    type="search"
+                    placeholder="Search"
+                    className='appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                />
+                <button className="bg-green-500 p-2 rounded-md mt-2">Search</button>
+            </div>
+        </div>
+
     )
 }
 
