ac64da8ab721ce109a88845c209551780b51983c
Kuwar Singh <66473902+Kuwar20@users.noreply.github.com>
2024-08-03 11:37:44 +0000

    feat:Add search feature in the fetch page

--- 15-sinlog/client/src/pages/Fetch.jsx
+++ 15-sinlog/client/src/pages/Fetch.jsx
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Fetch = () => {
+    const [search, setSearch] = useState('')
+
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        console.log(search)
+        const response = await fetch(`https://666fc0fe0900b5f872481dcc.mockapi.io/login`, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        const data = await response.json()
+        if (response.ok) {
+            console.log(data);
+        }
+    }
+
     return (
         <div>
             <h1>Fetch user data</h1>
             <div>
                 <h1>Search all</h1>
             </div>
-            <div>
-                <input
-                    type="search"
-                    placeholder="Search"
-                    className='appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
-                />
-                <button className="bg-green-500 p-2 rounded-md mt-2">Search</button>
-            </div>
+            <form onSubmit={handleSearch}>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        className='appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                    />
+                    <button
+                        className="bg-green-500 p-2 rounded-md mt-2"
+                    >Search
+                    </button>
+                </div>
+            </form>
         </div>
 
     )
