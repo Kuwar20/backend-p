36e8a4428999abeff9466b3da9ca8ed9b7c80a69
Kuwar Singh <66473902+Kuwar20@users.noreply.github.com>
2024-08-05 05:25:08 +0000

    feat:landing page and render modified and improved

--- 15-sinlog/client/src/pages/lifecycle.jsx
+++ 15-sinlog/client/src/pages/lifecycle.jsx
@@ -1,25 +1,3 @@
-// import React, { useState } from 'react';
-
-// const Counter = () => {
-//     // State hook
-//     const [count, setCount] = useState(0);
-
-//     // Handler to increment the count
-//     const increment = () => {
-//         setCount(count + 1);
-//     };
-
-//     // Render method
-//     return (
-//         <div>
-//             <p>Current Count: {count}</p>
-//             <button onClick={increment}>Increment</button>
-//         </div>
-//     );
-// };
-
-// export default Counter;
-
 import React, { Component } from 'react';
 
 class Counter extends Component {
