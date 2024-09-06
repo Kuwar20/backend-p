906b1308883ae62daa9280e4a30e0b6e601ef8e5
Kuwar20 <kuwarx1@gmail.com>
2024-09-06 21:20:46 +0530

    React init

--- 20-ecom-fs/frontend/src/pages/ProductDetail.jsx
+++ 20-ecom-fs/frontend/src/pages/ProductDetail.jsx
@@ -24,7 +24,7 @@ const ProductDetail = () => {
     if (loading) {
         return (
             <div className="flex justify-center items-center h-screen">
-                <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"></div>
+                <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-blue-500"></div>
             </div>
         );
     }
