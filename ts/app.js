8491edba1916fb440183c785b66a57dd2e25957e
Kuwar20 <kuwarx1@gmail.com>
2024-10-13 12:29:35 +0530

    Refactor app.ts to add a function for adding two numbers

--- ts/app.js
+++ ts/app.js
@@ -1,2 +1,11 @@
-var num = 10;
-console.warn(num);
+// const num:number=10;
+// const str:string='hello';
+// let isTrue:boolean=true;
+// console.warn(num);
+// console.warn(str);
+// console.warn(isTrue);
+function add(n1, n2) {
+    return n1 + n2;
+}
+var result = add(10, 20);
+console.log(result);
--- ts/app.ts
+++ ts/app.ts
@@ -1,3 +1,14 @@
-const num:number=10;
+// const num:number=10;
+// const str:string='hello';
+// let isTrue:boolean=true;
 
-console.warn(num);
+// console.warn(num);
+// console.warn(str);
+// console.warn(isTrue);
+
+function add(n1:number,n2:number):number{
+    return n1+n2;
+}
+
+let result = add(10,20);
+console.log(result);
