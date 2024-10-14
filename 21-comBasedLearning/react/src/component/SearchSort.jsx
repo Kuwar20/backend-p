d644e85dcc30d2e05ef36fe5184b29340a17c4d3
Kuwar20 <kuwarx1@gmail.com>
2024-10-14 23:16:26 +0530

    created names array and mapped it

--- 21-comBasedLearning/react/src/component/SearchSort.jsx
+++ 21-comBasedLearning/react/src/component/SearchSort.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const searchSort = () => {
+    const [names, setNames] = useState(["sagar", "kuwar", "sagari", "ayoo", "homeboy",
+        "john", "doe", "alice", "bob", "charlie",
+        "dave", "eva", "frank", "grace", "helen"]);
+
     return (
-        <div>searchSort</div>
+        <div className='flex justify-center items-center min-h-screen'>
+            <div>
+                <h1>Names List:</h1>
+                <ul>
+                    {names.map((name,index)=>(
+                        <li key={index}>{name}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     )
 }
 
