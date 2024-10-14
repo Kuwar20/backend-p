4b68a94768d1f25773766b8b54bf59f0d4e86561
Kuwar20 <kuwarx1@gmail.com>
2024-10-15 00:57:17 +0530

    added search within the array

--- 21-comBasedLearning/react/src/component/SearchSort.jsx
+++ 21-comBasedLearning/react/src/component/SearchSort.jsx
@@ -5,12 +5,37 @@ const searchSort = () => {
         "john", "doe", "alice", "bob", "charlie",
         "dave", "eva", "frank", "grace", "helen"]);
 
+    const [search, setSearch] = useState('');
+    const [result, setResult] = useState('');
+
+    const handleSearch = (e) => {
+        const query = e.target.value;
+        console.log(query)
+        setSearch(query)
+
+        const searchResult = names.filter((name)=>
+        name.toLowerCase().includes(query.toLowerCase())
+        )
+        
+        setResult(searchResult)
+    }
     return (
         <div className='flex justify-center items-center min-h-screen'>
             <div>
+                <div>
+                    <label>Search:</label>
+                    <input type="text"
+                        placeholder='Search Names'
+                        className='p-3 border rounded'
+                        value={search}
+                        onChange={handleSearch}
+                    />
+                    <h3>Result:</h3>
+                    {result}
+                </div>
                 <h1>Names List:</h1>
                 <ul>
-                    {names.map((name,index)=>(
+                    {names.map((name, index) => (
                         <li key={index}>{name}</li>
                     ))}
                 </ul>
