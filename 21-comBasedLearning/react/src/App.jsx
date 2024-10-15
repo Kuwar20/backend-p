052f00e284de55a82f93b432d2dddcbf3f0ff5d4
Kuwar20 <kuwarx1@gmail.com>
2024-10-15 23:19:22 +0530

    Searching Sorting Pagination added

--- 21-comBasedLearning/react/src/App.jsx
+++ 21-comBasedLearning/react/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SearchSort from './component/SearchSort';
 import SearchSort2 from './component/SearchSort2';
+import SearchSort3 from './component/SearchSort3';
 
 function App() {
 
@@ -9,7 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/ss" element={<SearchSort />} />
-          <Route path='/search' element={<SearchSort2 />}/>
+          <Route path='/search' element={<SearchSort2 />} />
+          <Route path='/search1' element={<SearchSort3 />} />
         </Routes>
       </BrowserRouter>
     </>
