0543e2dba8b9cc9fad6987a25f009d86dbcd09a5
Kuwar20 <kuwarx1@gmail.com>
2024-10-15 14:28:51 +0530

    name array and mapping

--- 21-comBasedLearning/react/src/component/SearchSort2.jsx
+++ 21-comBasedLearning/react/src/component/SearchSort2.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SearchSort2 = () => {
-  return (
-    <div>SearchSort2</div>
-  )
+    const namesList = ["sagar", "kuwar", "sagari", "ayoo", "homeboy",
+        "john", "doe", "alice", "bob", "charlie",
+        "dave", "eva", "frank", "grace", "helen"]
+    const [names] = useState(namesList)
+    return (
+        <div>
+            <div className='flex justify-center items-center min-h-screen bg-gray-100 p-5'>
+                <ul className='list-disc'>
+                    {names.map((name, index) => (
+                        <li key={index} className='mb-1'>{name}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default SearchSort2
