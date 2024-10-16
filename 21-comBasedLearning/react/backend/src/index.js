8b4c8a0ebcfc4e0bde75748674d2b219beffed8e
Kuwar20 <kuwarx1@gmail.com>
2024-10-16 21:31:40 +0530

    Add express backend

--- 21-comBasedLearning/react/backend/src/index.js
+++ 21-comBasedLearning/react/backend/src/index.js
@@ -4,6 +4,26 @@ import cors from "cors";
 const app = express();
 app.use(cors()); // Enable CORS for all routes
 
+import logger from '../logger.js';
+import morgan from 'morgan';
+
+const morganFormat = ':method :url :status :response-time ms';
+
+app.use(morgan(morganFormat, {
+    stream: {
+        write: (message) => {
+            const logObject = {
+                method: message.split(' ')[0],
+                url: message.split(' ')[1],
+                status: message.split(' ')[2],
+                responseTime: message.split(' ')[3],
+
+            };
+            logger.info(JSON.stringify(logObject));
+        }
+    }
+}));
+
 // Mock product data (simulating a database)
 const products = Array.from({ length: 50 }, (_, index) => ({
     id: index + 1,
