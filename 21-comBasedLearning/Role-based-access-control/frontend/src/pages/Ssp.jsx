db7456f6e46abadfe61d2e7e37e70a39583db170
Kuwar20 <kuwarx1@gmail.com>
2024-10-22 10:45:30 +0530

    Add Search

--- 21-comBasedLearning/Role-based-access-control/frontend/src/pages/Ssp.jsx
+++ 21-comBasedLearning/Role-based-access-control/frontend/src/pages/Ssp.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 const Ssp = () => {
 
   const [products, setProducts] = useState([])
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,11 +15,25 @@ const Ssp = () => {
     fetchData()
   }, [])
 
+  const searchedProduct = products.filter((product) =>
+    product.title.toLowerCase().includes(search.toLowerCase())
+)
+
   return (
     <div className='flex flex-col min-h-screen justify-center items-center p-6'>
+      <div>
+        <label>Search
+          <input type="text"
+            placeholder='Search Product'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className='m-2 p-3 border rounded'
+          />
+        </label>
+      </div>
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 w-full max-w-6xl'>
-        {products.length > 0 ? (
-          products.map((product) => (
+        {searchedProduct.length > 0 ? (
+          searchedProduct.map((product) => (
             <div key={product.id}>
               <h2>{product.title} :</h2>
               <p>{product.price} Rs</p>
