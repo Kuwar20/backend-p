f557ade47b9c2b1c51e32f056d69148aa69dee77
Kuwar20 <kuwarx1@gmail.com>
2024-10-25 12:44:52 +0530

    check the api

--- 21-comBasedLearning/Role-based-access-control/frontend/src/pages/Ssp9.jsx
+++ 21-comBasedLearning/Role-based-access-control/frontend/src/pages/Ssp9.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Ssp9 = () => {
-  return (
-    <div>Ssp9</div>
-  )
+    useEffect(() => {
+        const fetchData = async() => {
+            const response = await fetch(`https://fakestoreapi.com/products`);
+            const responseData = await response.json()
+            console.log(responseData)
+        }
+        fetchData();
+    })
+
+    return (
+        <div>Ssp9</div>
+    )
 }
 
 export default Ssp9
