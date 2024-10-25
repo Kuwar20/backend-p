edf089e52a9277f33dec786807585f89399a4e5e
Kuwar20 <kuwarx1@gmail.com>
2024-10-25 12:56:09 +0530

    Refactor component to fetch and display products

--- 21-comBasedLearning/Role-based-access-control/frontend/src/pages/Ssp9.jsx
+++ 21-comBasedLearning/Role-based-access-control/frontend/src/pages/Ssp9.jsx
@@ -1,18 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from "react";
 
 const Ssp9 = () => {
+    const [products, setProducts] = useState([]);
+
     useEffect(() => {
-        const fetchData = async() => {
+        const fetchData = async () => {
             const response = await fetch(`https://fakestoreapi.com/products`);
-            const responseData = await response.json()
-            console.log(responseData)
-        }
+            const responseData = await response.json();
+            setProducts(responseData);
+            console.log(responseData);
+        };
         fetchData();
-    })
+    },[]);
 
     return (
-        <div>Ssp9</div>
-    )
-}
+        <div>
+            <div></div>
+            <div>
+                {products.length > 0 ? (
+                    products.map((product) => (
+                        <div key={product.id}>
+                            <img src={product.image} alt={product.title} />
+                            <h3>{product.title}</h3>
+                        </div>
+                    ))
+                ) : (
+                    <h1>No Product Found</h1>
+                )}
+            </div>
+            <div></div>
+        </div>
+    );
+};
 
-export default Ssp9
+export default Ssp9;
